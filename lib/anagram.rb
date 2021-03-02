02cb175bb437b006c9a5e7ce335279c83362edca
Jessica Antunes <jessantunes@protonmail.com>
2021-03-02 12:14:35 -0500

    commit tree 6b1f80653588718c2250a23490508d3409511349

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+# require 'pry'
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+
+    def match(some_words)
+        anagrams = []
+
+        some_words.each do |words|
+            if words.split("").sort == @word.split("").sort
+                # binding.pry
+                anagrams<<words
+            end
+        end
+
+        anagrams
+    end
+
+    
+end
